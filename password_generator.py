4bbf54468b49e908a8d6cdc685b432c6f0d78fc6
Alireza Shirmohammad <alirezashirmohammad55@gmail.com>
2022-07-16 23:21:38 +0430

    Edite: Getting character number by user

--- password_generator.py
+++ password_generator.py
@@ -3,4 +3,8 @@ from random import sample
 
 characters = 'qwertyuiopasdfghjklzxcvbnm1234567890QWERTYUIOPASDFGHJKLZXCVBNM'
 
-print(''.join(sample(characters, 9)))
+letter_num = int(input('How many letters do you want?'))
+
+generated_password = ''.join(sample(characters, letter_num))
+
+print(generated_password)
